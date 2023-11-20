d2430f7e0223c5d9b756e832ac65ced599d20a62
Eduardo Machado <up202105337@g.uporto.pt>
2023-11-20 16:27:43 +0000

    login css complete

--- storage/framework/views/cd9fabb4c743061f07878868df3f8145.php
+++ storage/framework/views/cd9fabb4c743061f07878868df3f8145.php
@@ -1,36 +1,65 @@
 <?php $__env->startSection('content'); ?>
     <main class="flex-container">
-        <div class="sidebar">
-            <!-- Sidebar content -->
-            <a href="#">Home</a>
-            <a href=" <?php echo e(url('/search')); ?>">Explore</a>
-            <a href = "<?php echo e(url('/profile/'.Auth::user()->id)); ?>">Profile</a>
-            <a href="#">Notifications</a>
-            <a href="#">Settings</a>
-            <!-- Add more links as needed -->
-        </div>
+    <?php if(Auth::check()): ?>
+    <div class="sidebar">
+        <!-- Sidebar content -->
+        <a href="#">Home</a>
+        <a href="<?php echo e(url('/search')); ?>">Explore</a>
+        <a href="<?php echo e(url('/profile/' . Auth::user()->id)); ?>">Profile</a>
+        <a href="#">Notifications</a>
+        <a href="#">Settings</a>
+    </div>
+    <?php else: ?>
+    <div class="sidebar">
+        <!-- Sidebar content -->
+        <a href="<?php echo e(url('/login')); ?>">Home</a>
+        <a href="<?php echo e(url('/login')); ?>">Explore</a>
+        <a href="<?php echo e(url('/login')); ?>">Profile</a>
+        <a href="<?php echo e(url('/login')); ?>">Notifications</a>
+        <a href="<?php echo e(url('/login')); ?>">Settings</a>
+    </div>
+    <?php endif; ?>
 
         <div class="content">
-            <input type="text" id="search">
-            <div id="results-users"></div>
-            <div class="card-header"><?php echo e(__('Spaces')); ?></div>
+            <div class="card-header"><?php echo e(__('Public Spaces')); ?></div>
+            <div class="card-body">
+                <ul class="card-list">
+                    <?php $__currentLoopData = $publics; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $public): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <li><a href="/space/<?php echo e($public->id); ?>" class="card"><?php echo e($public->content); ?></a></li>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                </ul>
+            </div>
 
-            <div class="card-body">
-                <ul>
-                    <?php $__currentLoopData = $spaces; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $space): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                        <li><a href="/space/<?php echo e($space->id); ?>"><?php echo e($space->content); ?></a></li>
-                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-                </ul>
-            </div>
-            <div class="card-header"><?php echo e(__('My Spaces')); ?></div>
-            <div class="card-body">
-                <ul>
-                    <?php $__currentLoopData = $mines; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $mine): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                        <li><a href="/space/<?php echo e($mine->id); ?>"><?php echo e($mine->content); ?></a></li>
-                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-                </ul>
-                <?php echo $__env->make('partials.addSpace', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-            </div>
+            <?php if(Auth::check()): ?>
+                <div class="card-header"><?php echo e(__('Spaces')); ?></div>
+
+                <div class="card-body">
+                    <ul class="card-list">
+                        <?php $__currentLoopData = $spaces; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $space): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <li><a href="/space/<?php echo e($space->id); ?>" class="card"><?php echo e($space->content); ?></a></li>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </ul>
+                </div>
+
+                <div class="card-header"><?php echo e(__('My Spaces')); ?></div>
+                <div class="card-body">
+                    <ul class="card-list">
+                        <?php $__currentLoopData = $mines; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $mine): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <li><a href="/space/<?php echo e($mine->id); ?>" class="card"><?php echo e($mine->content); ?></a></li>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </ul>
+                </div>
+        </div>
+        <?php endif; ?>
+        <div class="searchbar">
+        <?php if(Auth::check()): ?>
+        <?php echo $__env->make('partials.addSpace', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+        <?php endif; ?>
+        <input type="text" id="search" placeholder="Search..." style="color: white;">
+        <div id="results-users"></div>
+        <?php if(Auth::check()): ?>
+        <div id="results-spaces"></div>
+        <?php endif; ?>
         </div>
     </main>
 <?php $__env->stopSection(); ?>
