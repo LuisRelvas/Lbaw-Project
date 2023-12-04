623c39007f4259af8c7e67e6476bc8e8ab8870e2
joaofguedes <up202108711@up.pt>
2023-12-04 18:20:46 +0000

    css-update-3

--- resources/views/pages/editUser.blade.php
+++ resources/views/pages/editUser.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="edit-user-container"
     <form method="POST" action="{{ url('profile/edit') }}" enctype="multipart/form-data">
         {{ csrf_field() }}
         <section class="edit-page-photo-options">
-                    <img id="edit-profile-photo" class="edit-page-image" src="{{ Auth::user()->media() }}" width=50% alt="Profile image">
-                    <h4 for="image">Choose a profile picture:</h4>
-                    <input type="file" name="image" id="image">
-                    <button form="removePhoto" class="edit-page-button">Remove Photo</button>
-                </section>
+            <h4 for="image"><i class="fa-solid fa-image"></i> Choose a profile picture:</h4>
+            <img id="edit-profile-photo" class="edit-page-image" src="{{ Auth::user()->media() }}" width=50%
+                alt="Profile image">
+            <input type="file" name="image" id="image">
+            <button form="removePhoto" class="edit-page-button">Remove Photo</button>
+        </section>
         <input type="hidden" name="user_id" value="{{ request()->route('id') }}">
-        <label for="name">Name</label>
+        <label for="name"><i class="fa-solid fa-user-pen"></i> Name</label>
         <input id="name" type="text" name="name" value="{{ old('name') }}">
         @if ($errors->has('name'))
             <span class="error">
@@ -18,7 +20,7 @@
             </span>
         @endif
 
-        <label for="email">E-Mail Address</label>
+        <label for="email"><i class="fa-solid fa-square-envelope"></i> E-Mail Address</label>
         <input id="email" type="email" name="email" value="{{ old('email') }}">
         @if ($errors->has('email'))
             <span class="error">
@@ -26,11 +28,11 @@
             </span>
         @endif
 
-        <label for="password">Password</label>
+        <label for="password"><i class="fa-solid fa-lock"></i> Password</label>
         @if (Auth::user()->isAdmin(Auth::user()))
-        <input id="password" type="password" name="password" >
-        @else 
-        <input id="password" type="password" name="password" required>
+            <input id="password" type="password" name="password">
+        @else
+            <input id="password" type="password" name="password" required>
         @endif
         @if ($errors->has('password'))
             <span class="error">
@@ -38,14 +40,14 @@
             </span>
         @endif
 
-        <label for="password-confirm">Confirm Password</label>
-        @if(Auth::user()->isAdmin(Auth::user()))
-        <input id="password-confirm" type="password" name="password_confirmation">
+        <label for="password-confirm"><i class="fa-solid fa-square-check"></i> Confirm Password</label>
+        @if (Auth::user()->isAdmin(Auth::user()))
+            <input id="password-confirm" type="password" name="password_confirmation">
         @else
-        <input id="password-confirm" type="password" name="password_confirmation" required>
+            <input id="password-confirm" type="password" name="password_confirmation" required>
         @endif
 
-        <label for="is_public">Private Profile</label>
+        <label for="is_public"><i class="fa-solid fa-key"></i> Private Profile</label>
         <input id="is_public" type="hidden" name="is_public" value="0">
         <input id="is_public" type="checkbox" name="is_public" value="1"
             {{ old('is_public') !== null ? 'checked' : '' }}>
@@ -57,9 +59,10 @@
 
         <div>
             <button type="submit">
-                Edit
+                Edit <i class="fa-solid fa-pen-to-square"></i>
             </button>
         </div>
     </form>
+    </div>
     @include('partials.footer')
 @endsection
--- resources/views/pages/user.blade.php
+++ resources/views/pages/user.blade.php
@@ -3,81 +3,92 @@
 @section('title', 'user')
 
 @section('content')
-<main class="flex-container">
-    @include('partials.sidebar')
+    <main class="flex-container">
+        @include('partials.sidebar')
 
-    <div class="userinfo" data-id="{{ $user->id }}">
+        <div class="userinfo" data-id="{{ $user->id }}">
 
-        <img src="{{ asset($user->media()) }}" class="profile-img" width="10%" style="border-radius: 50%; padding: 1em" alt="profile media">
-        <div class="user">
-            <p><a href="/profile/{{ $user->id }}">{{ $user->name }}</a></p>
+            <div class="profile-container">
+                <img src="{{ asset($user->media()) }}" class="profile-img" width="10%"
+                    style="border-radius: 50%; padding: 1em" alt="profile media">
+                <div class="profile-info">
+                    <div class="user">
+                        <p><a href="/profile/{{ $user->id }}">{{ $user->name }}</a></p>
+                        <div class="username">
+                            <p>@ {{ $user->username }}</p>
+                        </div>
+                        @if (Auth::check())
+                            <p>Following: {{ $countFollows }}</p>
+                            <p>Followers: {{ $countFollowers }}</p>
+                        @endif
+                    </div>
+                </div>
+            </div>
             @if (Auth::check())
-            <p>Following: {{ $countFollows }}</p>
-            <p>Followers: {{ $countFollowers }}</p>
-            @endif
-        </div>
-        <div class="username">
-            <p>@ {{ $user->username }}</p>
-        </div>
-        @if (Auth::check())
-            @if ($user->id == Auth::User()->id || Auth::User()->isAdmin(Auth::User()))
-                <div class="button-container"><a class="button" href="/profile/{{ $user->id }}/editUser">Edit
-                        Profile</a>
+                @if ($user->id == Auth::User()->id || Auth::User()->isAdmin(Auth::User()))
+                    <div class="button-container"><a class="button" href="/profile/{{ $user->id }}/editUser">Edit
+                            Profile <i class="fa-solid fa-user-pen"></i></a>
                         <button id="deleteProfile{{ $user->id }}" onclick="deleteProfile({{ $user->id }})"
-                            class="button-user">&#10761;
-                            <div><i class="cross"></i></div>
+                            class="button-user">
+                            <i class="fa-solid fa-trash"></i>
                         </button>
                         <a class ="button"
-                        href="{{ Auth::check() && Auth::user()->isAdmin(Auth::user()) ? url('/admin') : url('/homepage') }}">Back
-                        to
-                        home page</a>
-                </div>
-                @if (Auth::User()->isAdmin(Auth::User()))
-                    @if (!$isBlocked)
-                        <form method="POST" action="/profile/block/{{ $user->id }}">
-                            @csrf
-                            <button type="submit">Block</button>
-                        </form>
+                            href="{{ Auth::check() && Auth::user()->isAdmin(Auth::user()) ? url('/admin') : url('/homepage') }}">
+                            <i class="fa-solid fa-arrow-left"></i> <i class="fa-solid fa-house"></i>
+                        </a>
+                    </div>
+                    @if (Auth::User()->isAdmin(Auth::User()))
+                        @if (!$isBlocked)
+                            <form method="POST" action="/profile/block/{{ $user->id }}">
+                                @csrf
+                                <button type="submit">Block</button>
+                            </form>
+                        @else
+                            <form method="POST" action="/profile/unblock/{{ $user->id }}">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit">Unblock</button>
+                            </form>
+                        @endif
+                    @endif
+                @else
+                    @if (Auth::check() && Auth::user()->id != $user->id && $user->deleted == false)
+                        <button id="profileState{{ $user->id }}" class="profile-interaction-button"
+                            onclick="changeProfileState({{ $user->id }},{{ Auth::user()->id }},{{ $user->is_public }})">
+                            @if (Auth::user()->isFollowing($user))
+                                <i id="text-icon" aria-hidden="true"></i> Unfollow
+                            @elseif(Auth::user()->hasSentFollowRequest($user))
+                                <i id="text-icon" aria-hidden="true"></i> Pending
+                            @else
+                                <i id="text-icon" aria-hidden="true"></i> Follow
+                            @endif
+                        </button>
                     @else
-                        <form method="POST" action="/profile/unblock/{{ $user->id }}">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit">Unblock</button>
-                        </form>
+                        <h3>Account Deleted</h3>
                     @endif
                 @endif
-            @else
-                @if(Auth::check() && Auth::user()->id != $user->id && $user->deleted == false)
-                    <button id="profileState{{$user->id}}" class="profile-interaction-button" onclick="changeProfileState({{$user->id}},{{Auth::user()->id}},{{$user->is_public}})">
-                        @if(Auth::user()->isFollowing($user)) <i id="text-icon" aria-hidden="true"></i> Unfollow
-                        @elseif(Auth::user()->hasSentFollowRequest($user)) <i id="text-icon" aria-hidden="true"></i> Pending
-                        @else <i id="text-icon" aria-hidden="true"></i> Follow
-                        @endif
-                    </button>
-                @else
-                    <h3>Account Deleted</h3>
-                @endif
             @endif
-        @endif
-        @if(!empty($wants))
-        @foreach($wants as $want)
-            @if(Auth::check() && $want->user_id2 == Auth::user()->id)
-            <p>Follow request from {{ $want->user_id1 }}</p>
-            <button onclick="declineFollowRequest({{ $want->user_id1 }}, {{ $want->user_id2 }})" class="button-join-delete">&#10761;
-                <div><i class="cross"></i></div>
-                </button>
-                <button onclick="acceptFollowRequest({{ $want->user_id1 }}, {{ $want->user_id2 }})" class="button-join-accept"> &#10003;
-                    <div><i class="tick"></i></div>
-                </button>  
+            @if (!empty($wants))
+                @foreach ($wants as $want)
+                    @if (Auth::check() && $want->user_id2 == Auth::user()->id)
+                        <p>Follow request from {{ $want->user_id1 }}</p>
+                        <button onclick="declineFollowRequest({{ $want->user_id1 }}, {{ $want->user_id2 }})"
+                            class="button-join-delete">&#10761;
+                            <div><i class="cross"></i></div>
+                        </button>
+                        <button onclick="acceptFollowRequest({{ $want->user_id1 }}, {{ $want->user_id2 }})"
+                            class="button-join-accept"> &#10003;
+                            <div><i class="tick"></i></div>
+                        </button>
+                    @endif
+                @endforeach
             @endif
-        @endforeach
-        @endif
-        @if (session('success'))
-            <p class="success">
-                {{ session('success') }}
-            </p>
-        @endif
-    </div>
-</main>
-@include('partials.footer')
+            @if (session('success'))
+                <p class="success">
+                    {{ session('success') }}
+                </p>
+            @endif
+        </div>
+    </main>
+    @include('partials.footer')
 @endsection
