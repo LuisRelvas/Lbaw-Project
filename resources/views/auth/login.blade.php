c94c433acc5c8dcfd413fc57f64abdf7e8eba756
joaofguedes <up202108711@up.pt>
2023-12-05 11:24:02 +0000

    changed login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,40 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-    <form id="loginForm" method="POST" action="{{ route('login') }}">
-        {{ csrf_field() }}
-        <label for="email"><i class="fa-solid fa-square-envelope"></i> E-mail</label>
-        <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
-        @if ($errors->has('email'))
-            <span class="error">
-                {{ $errors->first('email') }}
-            </span>
-        @endif
+    <div class="login-container">
+        <div class="login-card">
+            <form id="loginForm" method="POST" action="{{ route('login') }}">
+                {{ csrf_field() }}
+                <label for="email"><i class="fa-solid fa-square-envelope"></i> E-mail</label>
+                <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
+                @if ($errors->has('email'))
+                    <span class="error">
+                        {{ $errors->first('email') }}
+                    </span>
+                @endif
 
-        <label for="password"><i class="fa-solid fa-lock"></i> Password</label>
-        <input id="password" type="password" name="password" required>
-        @if ($errors->has('password'))
-            <span class="error">
-                {{ $errors->first('password') }}
-            </span>
-        @endif
+                <label for="password"><i class="fa-solid fa-lock"></i> Password</label>
+                <input id="password" type="password" name="password" required>
+                @if ($errors->has('password'))
+                    <span class="error">
+                        {{ $errors->first('password') }}
+                    </span>
+                @endif
 
-        <label>
-            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-        </label>
+                <label>
+                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                </label>
 
-        <button type="submit">
-            Login <i class="fa-solid fa-right-to-bracket"></i>
-        </button>
-        <a class="button" href="{{ route('register') }}">Register <i class="fa-solid fa-pen-to-square"></i></a>
-        @if (session('success'))
-            <p class="success">
-                {{ session('success') }}
-            </p>
-        @endif
-    </form>
+                <button type="submit">
+                    Login <i class="fa-solid fa-right-to-bracket"></i>
+                </button>
+                <a class="button" href="{{ route('register') }}">Register <i class="fa-solid fa-pen-to-square"></i></a>
+                @if (session('success'))
+                    <p class="success">
+                        {{ session('success') }}
+                    </p>
+                @endif
+            </form>
 
-
+        </div>
     </div>
     @include('partials.footer')
 @endsection
