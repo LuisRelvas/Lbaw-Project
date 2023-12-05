3082989c47afced0590804f9727d16f72a956b4d
Eduardo Machado <up202105337@g.uporto.pt>
2023-12-05 19:59:37 +0000

    fixing admin feature with profile feature

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -41,9 +41,13 @@ class RegisterController extends Controller
         ]);
 
         $credentials = $request->only('email', 'password');
-        Auth::attempt($credentials);
-        $request->session()->regenerate();
-        return redirect('/homepage')
+        //Auth::attempt($credentials);
+        if(Auth::user()->isAdmin($user)){
+            return redirect('/admin')
+                ->withSuccess('You have successfully created an account');}
+        else{
+                $request->session()->regenerate();
+                return redirect('/homepage')
             ->withSuccess('You have successfully registered & logged in!');
-    }
+        }
 }
