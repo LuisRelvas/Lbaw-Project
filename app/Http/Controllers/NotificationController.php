9ee6adb33be5cc0cee56eae89e5cc3bc925fc9db
LuisRelvas <luisrelvas@netcabo.pt>
2023-12-08 16:39:23 +0000

    Fixing merge conflicts

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -31,9 +31,42 @@ class NotificationController extends Controller
         $commentNotifications = CommentNotification::whereIn('id', $notificationsIds)->get();
         $groupNotifications = GroupNotification::whereIn('id', $notificationsIds)->get();
         $notifications = $userNotifications->merge($spaceNotifications)->merge($commentNotifications)->merge($groupNotifications);
-        return view('pages.notification', [
-            'notifications' => $notifications
-        ]);
+        $final = [];
+        foreach($notifications as $notification) 
+        {
+            if($notification->user_id) 
+            {
+                
+                $who = User::select('name')->where('id',$notification->user_id)->first();
+                $who = $who->name;
+                $not = Notification::select('emits_user')->where('id',$notification->id)->first();
+                $link = '/profile/'.$not->emits_user;
+            }
+            if($notification->group_id) 
+            {
+                $who = Group::select('name')->where('id',$notification->group_id)->first();
+                $who = $who->name;
+                $link = '/group/'.$notification->group_id;
+            }
+            if($notification->space_id) 
+            {
+                $who = Space::select('content')->where('id',$notification->space_id)->first();
+                $who = $who->content;
+                $link = '/space/'.$notification->space_id;
+            }
+            if($notification->comment_id) 
+            {
+                $who = Comment::select('content')->where('id',$notification->comment_id)->first();
+                $who = $who->content;
+                $space = Comment::select('space_id')->where('id',$notification->comment_id)->first();
+                $link = '/space/'.$space->space_id;
+            }
+            $type = $notification->notification_type;
+            $aux = Notification::select('emits_user')->where('id',$notification->id)->first();
+            $name = User::select('name')->where('id',$aux->emits_user)->first();
+            array_push($final,[$type,$name,$who,$link,$notification->id]);
+        }
+        return response()->json($final);
     }
 
     public function edit(int $id) 
