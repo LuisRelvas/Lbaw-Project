afc2be044303dd9160f036233639a24e42f6591f
joaofguedes <up202108711@up.pt>
2023-12-09 16:59:06 +0000

    update spaces: most recent to least recent

--- resources/views/pages/space.blade.php
+++ resources/views/pages/space.blade.php
@@ -38,18 +38,18 @@
                 @endif
                 @if ((Auth::check() && $space->user_id == Auth::user()->id) || (Auth::check() && Auth::user()->isAdmin(Auth::user())))
                     <button id="deleteSpace{{ $space->id }}" onclick="deleteSpace({{ $space->id }})"
-                        class="button-space-comment">&#10761;
-                        <div><i class="cross"></i></div>
+                        class="button-space-comment">
+                        <i class="fa-solid fa-trash"></i>
                     </button>
                 @endif
                 @if ((Auth::check() && $space->user_id == Auth::user()->id) || (Auth::check() && Auth::user()->isAdmin(Auth::user())))
                     <button id="editSpace{{ $space->id }}" onclick="editSpace({{ $space->id }})"
                         class="button-space-comment">
-                        <div id="text-config"><i id="text-icon" class="pencil"></i></div>
+                        <i class="fa-solid fa-pen-to-square"></i>
                     </button>
                     <button id="cancelEditSpace{{ $space->id }}" onclick="cancelEditSpace({{ $space->id }})"
                         style="visibility:hidden;" class="button-space-comment">
-                        <div><i class="cross"></i> </div>
+                        <div>Cancel</div>
                     </button>
                 @endif
                 @if (session('success'))
