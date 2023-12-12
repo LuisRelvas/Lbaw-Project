7044cc49ad9f01b29f163aa4ab3f5fc0f7387561
LuisRelvas <luisrelvas@netcabo.pt>
2023-12-12 17:42:06 +0000

    Joining the search exact with the FTS

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -4,25 +4,25 @@
     @include('partials.sidebar');
     <div class="search-card">
         <h1><i class="fa-solid fa-magnifying-glass"></i> Explore</h1>
-        <button onclick="handlePrincipal()">Principal</button>
-        <button onclick="handleUsers()">Users</button>
-        <button onclick="handleSpaces()">Spaces</button>
-        <button onclick="handleComments()">Comments</button>
-        <button onclick="handleGroups()">Groups</button>
         <form action="{{ url('homepage/search') }}" method="get">           
-        <input type="text" id="search" name="search" placeholder="Explore..." style="color: white;"
-                pattern="[a-zA-Z0-9\s]+">
-                <div id="filters" style="display: none;">
-                    <input type="date" id="date" name="date">
-                    <input type="radio" id="publicRadio" name="profileType" value="public">Only Public Profiles
-                    <input type="radio" id="privateRadio" name="profileType" value="private">Only Private Profiles
-                    <input type="radio" id="publicRadioSpace" name="spaceType" value="public">Only Public Spaces
-                    <input type="radio" id="privateRadioSpace" name="spaceType" value="private">Only Private Spaces
-                    <input type="radio" id="publicRadioGroup" name="groupType" value="public">Only Public Groups
-                    <input type="radio" id="privateRadioGroup" name="groupType" value="private">Only Private Groups
-                </div>  
+            <div class="searchbar">
+            <input type="text" id="search" name="search" placeholder="Search..." style="color: white;" pattern="[a-zA-Z0-9\s]+" oninput="performFTS()">                <div id="results-users"></div>
+                    <div id="results-users"></div>
+                    <div id="results-spaces"></div>
+                    <div id="results-groups"></div>
+                    <div id="results-comments"></div>
+            </div>
+            <div id="filters" style="display: none;">
+                <input type="date" id="date" name="date">
+                <input type="radio" id="publicRadio" name="profileType" value="public">Only Public Profiles
+                <input type="radio" id="privateRadio" name="profileType" value="private">Only Private Profiles
+                <input type="radio" id="publicRadioSpace" name="spaceType" value="public">Only Public Spaces
+                <input type="radio" id="privateRadioSpace" name="spaceType" value="private">Only Private Spaces
+                <input type="radio" id="publicRadioGroup" name="groupType" value="public">Only Public Groups
+                <input type="radio" id="privateRadioGroup" name="groupType" value="private">Only Private Groups
+            </div>  
             <button type="button" onclick="toggleFilters()">Filters <i class="fa-solid fa-filter"></i></button>
-            <button type="submit" onclick="handlePrincipal();">Search <i class="fa-solid fa-magnifying-glass"></i></button>        
+            <button type="submit">Search <i class="fa-solid fa-magnifying-glass"></i></button>        
         </form>
         <div id="users" class="search-page-results">
             @if (isset($users) && !$users->isEmpty())
