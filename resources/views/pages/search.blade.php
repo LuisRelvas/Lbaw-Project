47f92c27a0202136342ed8b99c816d80dcf531b1
joaofguedes <up202108711@up.pt>
2023-12-14 01:24:03 +0000

    update search-page (strange bug with searchbar)

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -1,67 +1,71 @@
 @extends('layouts.app')
 @section('content')
-<div class="search-page-container">
-    @include('partials.sidebar');
-    <div class="search-card">
-        <h1><i class="fa-solid fa-magnifying-glass"></i> Explore</h1>
-        <form action="{{ url('homepage/search') }}" method="get">           
-            <div class="searchbar">
-            <input type="text" id="search" name="search" placeholder="Search..." style="color: white;" pattern="[a-zA-Z0-9\s]+"  onclick="showResultsContainer()" onblur="hideResultsContainer()" autocomplete="off">         
-                <div class="results-container" id="resultsContainer">
-                    <div id="results-users"></div>
-                    <div id="results-spaces"></div>
-                    <div id="results-groups"></div>
-                    <div id="results-comments"></div>
+    <div class="search-page-container">
+        @include('partials.sidebar');
+        <div class="search-card">
+            <h1><i class="fa-solid fa-magnifying-glass"></i> Explore</h1>
+            <form action="{{ url('homepage/search') }}" method="get">
+                <div class="searchbar">
+                    <input type="text" id="search" name="search" placeholder="Search..." style="color: white;"
+                        pattern="[a-zA-Z0-9\s]+" onclick="showResultsContainer()" onblur="hideResultsContainer()"
+                        autocomplete="off">
+                    <div class="results-container" id="resultsContainer">
+                        <div id="results-users"></div>
+                        <div id="results-spaces"></div>
+                        <div id="results-groups"></div>
+                        <div id="results-comments"></div>
+                    </div>
                 </div>
-            </div>
-            <div id="filters" style="display: none;">
-                <input type="date" id="date" name="date">
-                <input type="radio" id="publicRadio" name="profileType" value="anyone">Anyone
-                <input type="radio" id="privateRadio" name="profileType" value="follow">People that You follow
-            </div>  
-            <button type="button" onclick="toggleFilters()">Filters <i class="fa-solid fa-filter"></i></button>
-            <button type="submit">Search <i class="fa-solid fa-magnifying-glass"></i></button>        
-        </form>
-        
-        <!-- Show results only if the search input is not empty -->
-        <div id="users" class="search-page-results" style="overflow-y: auto;">
-            @if (isset($users) && !$users->isEmpty())
-                @foreach ($users as $user)
-                    <h2><a href="/profile/{{ $user->id }}">{{ $user->username }}</a></h2>
-                @endforeach
-            @else
-                <p>No results found for your search</p>
-            @endif
-        </div>
-        <div id="spaces" class="search-page-results">
-            @if (isset($spaces) && !$spaces->isEmpty())
-                @foreach ($spaces as $space)
-                    <h2><a href="/space/{{ $space->id }}">{{ $space->content }}</a></h2>
-                @endforeach
-            @else
-                <p>No results found for your search</p>
-            @endif
-        </div>
+                <div id="filters" style="display: none;">
+                    <input type="date" id="date" name="date">
+                    <input type="radio" id="publicRadio" name="profileType" value="anyone">
+                    <label for="publicRadio" class="radio-name">Anyone</label>
+                    <input type="radio" id="privateRadio" name="profileType" value="follow">
+                    <label for="privateRadio" class="radio-name">People that You follow</label>
+                </div>
+                <button type="button" onclick="toggleFilters()">Filters <i class="fa-solid fa-filter"></i></button>
+                <button type="submit">Search <i class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
 
-        <div id="comments" class="search-page-results">
-            @if (isset($comments) && !$comments->isEmpty())
-                @foreach ($comments as $comment)
-                    <h2><a href="/space/{{ $comment->space_id }}">{!! strip_tags($comment->content) !!}</a></h2>
-                @endforeach
-            @else
-                <p>No results found for your search</p>
-            @endif
-        </div>
-        <div id="groups" class="search-page-results">
-            @if (isset($groups) && !$groups->isEmpty())
-                @foreach ($groups as $group)
-                    <h2><a href="/group/{{ $group->id }}">{{ $group->name }}</a></h2>
-                @endforeach
-            @else
-                <p>No results found for your search</p>
-            @endif
+            <!-- Show results only if the search input is not empty -->
+            <div id="users" class="search-page-results" style="overflow-y: auto;">
+                @if (isset($users) && !$users->isEmpty())
+                    @foreach ($users as $user)
+                        <h2><a href="/profile/{{ $user->id }}">{{ $user->username }}</a></h2>
+                    @endforeach
+                @else
+                    <p>No results found for your search</p>
+                @endif
+            </div>
+            <div id="spaces" class="search-page-results" style="overflow-y: auto;">
+                @if (isset($spaces) && !$spaces->isEmpty())
+                    @foreach ($spaces as $space)
+                        <h2><a href="/space/{{ $space->id }}">{{ $space->content }}</a></h2>
+                    @endforeach
+                @else
+                    <p>No results found for your search</p>
+                @endif
+            </div>
+
+            <div id="comments" class="search-page-results" style="overflow-y: auto;">
+                @if (isset($comments) && !$comments->isEmpty())
+                    @foreach ($comments as $comment)
+                        <h2><a href="/space/{{ $comment->space_id }}">{!! strip_tags($comment->content) !!}</a></h2>
+                    @endforeach
+                @else
+                    <p>No results found for your search</p>
+                @endif
+            </div>
+            <div id="groups" class="search-page-results" style="overflow-y: auto;">
+                @if (isset($groups) && !$groups->isEmpty())
+                    @foreach ($groups as $group)
+                        <h2><a href="/group/{{ $group->id }}">{{ $group->name }}</a></h2>
+                    @endforeach
+                @else
+                    <p>No results found for your search</p>
+                @endif
+            </div>
         </div>
     </div>
-</div>
-@include('partials.footer')
+    @include('partials.footer')
 @endsection
