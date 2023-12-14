1dfd291224794398570754aacd0cceb0dee81dfe
joaofguedes <up202108711@up.pt>
2023-12-14 22:14:48 +0000

    search

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -40,36 +40,7 @@
                 </ul>
             </div>
         </div>
-        <div class="searchbar">
-            <input type="text" id="search" placeholder="Search..." style="color: white;" pattern="[a-zA-Z0-9\s]+">
-            <div id="results-users"></div>
-            @if (Auth::check())
-                <div id="results-spaces"></div>
-                <div id="results-groups"></div>
-                <div id="results-comments"></div>
-            @endif
-            @if (Auth::check())
-                @if (session('success'))
-                    <p class="success">
-                        {{ session('success') }}
-                    </p>
-                @endif
-            @endif
-
-            <div class ="trend-content">
-                <h2>Trending</h2>
-                <div class="trend">
-                    @if (Auth::check())
-                        @foreach ($trends as $trend)
-                            @php
-                                $real_space = \App\Models\Space::findOrFail($trend->space_id);
-                            @endphp
-                            <a href="/space/{{ $trend->space_id }}" class="trend-card">{{ $real_space->content }}</a>
-                        @endforeach
-                    @endif
-                </div>
-            </div>
-        </div>
+        @include('partials.sideSearchbar')
 
     </main>
     @include('partials.footer')
