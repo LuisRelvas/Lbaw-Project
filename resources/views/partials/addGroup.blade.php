695ea0093dbfd62cbdf915063e8b9ad0cf2f05a6
LuisRelvas <luisrelvas@netcabo.pt>
2023-12-16 01:58:15 +0000

    Starting working better on the edit User

--- resources/views/partials/addGroup.blade.php
+++ resources/views/partials/addGroup.blade.php
@@ -1,8 +1,7 @@
+<script type="text/javascript" src={{ url('js/groups.js') }} defer></script>
 <button id="openFormButton" class="fas fa-plus"></button>
 
-<!-- The Modal -->
 <div id="myModal" class="modal">
-  <!-- Modal content -->
   <div class="modal-content">
     <span class="close">&times;</span>
     <form method="POST" action="{{ url('/group/add') }}" enctype="multipart/form-data">
@@ -31,40 +30,3 @@
   </div>
 </div>
 
-<!-- JavaScript to handle the button click -->
-<script>
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("openFormButton");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-  setTimeout(function() {
-    modal.style.opacity = "1";
-  }, 50); // Delay to ensure the modal is visible before starting the transition
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.opacity = "0";
-  setTimeout(function() {
-    modal.style.display = "none";
-  }, 500); // Delay to ensure the transition finishes before hiding the modal
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.opacity = "0";
-    setTimeout(function() {
-      modal.style.display = "none";
-    }, 500); // Delay to ensure the transition finishes before hiding the modal
-  }
-}
-</script>
