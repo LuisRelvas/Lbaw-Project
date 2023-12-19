34186b41fdfafd7f5ea12d877da96bb28b3d3a6a
Eduardo Machado <up202105337@g.uporto.pt>
2023-12-19 18:21:47 +0000

    profile done, review needed in the ecplore tab

--- resources/views/partials/userCard.blade.php
+++ resources/views/partials/userCard.blade.php
@@ -1,67 +1,86 @@
 <div class="userinfo" data-id="{{ $user->id }}">
 
     <div class="profile-container">
-        <img src="{{ asset($user->media()) }}" class="profile-img" width="10%" style="border-radius: 50%; padding: 1em"
+        <img src="{{ asset($user->media()) }}" class="profile-img" width="20%" style="border-radius: 50%; padding: 1em"
             alt="profile media">
-        <div class="profile-info">
-            <div class="user" id="user{{$user->id}}">
-                <p><a href="/profile/{{ $user->id }}">{{ $user->username }}</a></p>
-                <div class="name">
-                    <div class="name">{{ $user->name }}</div>
+
+        <div class="user-card-container" style="display: flex; justify-content: space-between;">
+        <div class="user-card" style="flex: 1;">
+                <div class="user" id="user{{$user->id}}">
+                    <div class="name">
+                        <div class="name">{{ $user->name }}</div>
+                    </div>
+                    <p1><a href="/profile/{{ $user->id }}">{{ $user->username }}</a></p>
+                    <p>
+                    <div class="email">{{ $user->email }}</div>
+                    </p>
+                    @if (Auth::check() && Auth::user()->id == $user->id)
                 </div>
-                <p>
-                <div class="email">{{ $user->email }}</div>
-                </p>
-                @if (Auth::check() && Auth::user()->id == $user->id)
-                <p>Following: <a href="/profile/{{$user->id}}/following">{{ $countFollows }}</a></p>
-                <p>Followers: <a href="/profile/{{$user->id}}/followers">{{ $countFollowers }}</a>
-                </p>
                 @endif
+            </div>    
+        <div class="user-card" style="flex: 1; margin-right: 20px;">
+                <div class="user-header">
+                    <div style="display: flex; justify-content: space-between;">
+                        <div style="display: flex; flex-direction: column; text-align: center; color: white; font-size: larger; margin-right: 20px;">
+                            <span>Following</span>
+                            <a href="/profile/{{$user->id}}/following" style="color: white;">{{ $countFollows }}</a>
+                        </div>
+                        <div style="display: flex; flex-direction: column; text-align: center; color: white; font-size: larger; margin-right: 20px;">
+                            <span>Followers</span>
+                            <a href="/profile/{{$user->id}}/followers" style="color: white;">{{ $countFollowers }}</a>
+                        </div>
+                        <div style="display: flex; flex-direction: column; text-align: center; color: white; font-size: larger;">
+                            <span>Spaces</span>
+                            <span>{{ count($spaces) }}</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
     @if (Auth::check())
     @if ($user->id == Auth::User()->id || Auth::User()->isAdmin(Auth::User()))
-    <button id="editUser{{ $user->id }}" onclick="editUser({{ $user->id }},{{$user->is_public}})"
-        class="button-user-comment">
-        &#9998;
-        <div id="text-config"><i id="text-icon" class="pencil"></i></div>
-    </button>
-    <button id="cancelEditUser{{ $user->id }}" style="visibility: hidden;"
-        onclick="cancelEditUser({{ $user->id }})">Cancel</button>
-    @include ('partials.editUser')
-    <a class="button" href="/profile/{{ $user->id }}/editUser/password">Change Password <i
-            class="fa-solid fa-key"></i></a>
-    <button id="deleteProfile{{ $user->id }}" onclick="deleteProfile({{ $user->id }})" class="button-user">
-        <i class="fa-solid fa-trash"></i>
-    </button>
-    
-    @if (Auth::User()->isAdmin(Auth::User()))
-    @if (!$isBlocked)
-    <form method="POST" action="/profile/block/{{ $user->id }}">
-        @csrf
-        <button type="submit">Block</button>
-    </form>
-    @else
-    <form method="POST" action="/profile/unblock/{{ $user->id }}">
-        @csrf
-        @method('DELETE')
-        <button type="submit">Unblock</button>
-    </form>
-    @endif
-    @endif
-    @else
-    @if (Auth::check() && Auth::user()->id != $user->id && $user->id != 1)
-    <button id="profileState{{ $user->id }}" class="profile-interaction-button"
-        onclick="changeProfileState({{ $user->id }},{{ Auth::user()->id }},{{ $user->is_public }})">
-        @if (Auth::user()->isFollowing($user))
-        <i id="text-icon" aria-hidden="true"></i> Unfollow
-        @elseif(Auth::user()->hasSentFollowRequest($user))
-        <i id="text-icon" aria-hidden="true"></i> Pending
+    <div class="button-container-user">
+        <button id="editUser{{ $user->id }}" onclick="editUser({{ $user->id }},{{$user->is_public}})"
+            class="button-user-comment">
+            <i class="fa-solid fa-pen"></i>
+        </button>
+        <button id="cancelEditUser{{ $user->id }}" style="visibility: hidden;"
+            onclick="cancelEditUser({{ $user->id }})">Cancel</button>
+        @include ('partials.editUser')
+        <button id="button" href="/profile/{{ $user->id }}/editUser/password">Change Password <i
+                class="fa-solid fa-key"></i></button>
+        <button id="deleteProfile{{ $user->id }}" onclick="deleteProfile({{ $user->id }})" class="button-user">
+            <i class="fa-solid fa-trash"></i>
+        </button>
+        </div>
+        @if (Auth::User()->isAdmin(Auth::User()))
+        @if (!$isBlocked)
+        <form method="POST" action="/profile/block/{{ $user->id }}">
+            @csrf
+            <button type="submit">Block</button>
+        </form>
         @else
-        <i id="text-icon" aria-hidden="true"></i> Follow
+        <form method="POST" action="/profile/unblock/{{ $user->id }}">
+            @csrf
+            @method('DELETE')
+            <button type="submit">Unblock</button>
+        </form>
         @endif
-    </button>
+        @endif
+        @else
+        @if (Auth::check() && Auth::user()->id != $user->id && $user->id != 1)
+        <button id="profileState{{ $user->id }}" class="profile-interaction-button"
+            onclick="changeProfileState({{ $user->id }},{{ Auth::user()->id }},{{ $user->is_public }})">
+            @if (Auth::user()->isFollowing($user))
+            <i id="text-icon" aria-hidden="true"></i> Unfollow
+            @elseif(Auth::user()->hasSentFollowRequest($user))
+            <i id="text-icon" aria-hidden="true"></i> Pending
+            @else
+            <i id="text-icon" aria-hidden="true"></i> Follow
+            @endif
+        </button>
+    
     @else
     <h3>Account Deleted</h3>
     @endif
